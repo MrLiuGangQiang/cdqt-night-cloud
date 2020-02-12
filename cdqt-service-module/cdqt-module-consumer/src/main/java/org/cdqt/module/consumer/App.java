8a7fdf97e5c5ddf9e2de95340a48d61913f3dfec
LiuGangQiang <LiuGangQiang@LiuGangQiang>
2020-02-12 23:50:16 +0800

    调整项目目录结构

---
+++ cdqt-service-module/cdqt-module-consumer/src/main/java/org/cdqt/module/consumer/App.java
@@ -0,0 +1,13 @@
+package org.cdqt.module.consumer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
