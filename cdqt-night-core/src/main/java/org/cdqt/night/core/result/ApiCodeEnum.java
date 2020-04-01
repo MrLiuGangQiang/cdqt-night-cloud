db4f8f9607f31a029f1bd5f2ba075db7b8753ec9
LiuGangQiang <LiuGangQiang@LiuGangQiang>
2020-04-01 22:18:17 +0800

    修改资源文件名

--- cdqt-night-core/src/main/java/org/cdqt/night/core/result/ApiCodeEnum.java
+++ cdqt-night-core/src/main/java/org/cdqt/night/core/result/ApiCodeEnum.java
@@ -115,6 +115,6 @@ public enum ApiCodeEnum {
 	 */
 	ApiCodeEnum(int value, String message) {
 		this.value = value;
-		this.message = Prompt.bundle("i18n.night_core", message, Locale.getDefault());
+		this.message = Prompt.bundle("i18n.core", message, Locale.getDefault());
 	}
 }
